491e315f1bbecc7ee579917200628fa60d8df04b
Prabhat Khera <prabhat.khera@9spokes.com>
2020-10-19 11:31:15 +1300

    script tag updated

--- src/Snippets.js
+++ src/Snippets.js
@@ -17,7 +17,7 @@ const Snippets = {
       (function(w,d,s,l,i){w[l]=w[l]||[];
         w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
         var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+'${gtm_auth}${gtm_preview}&gtm_cookies_win=x';
+        j.async=true;j.src='https://www.googletagmanager.com/gtag/js?id='+i+dl+'${gtm_auth}${gtm_preview}&gtm_cookies_win=x';
         f.parentNode.insertBefore(j,f);
       })(window,document,'script','${dataLayerName}','${id}');`
   
